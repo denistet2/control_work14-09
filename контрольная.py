840167a9550c36890dc8b6bdbd9081e848fd2659
denistet2 <denistet@gmail.com>
2022-09-14 13:58:42 +0300

    1

--- контрольная.py
+++ контрольная.py
@@ -1,4 +1,4 @@
-def calculation(a,b):
+def calculation():
     _summ = a + b
     _razn = a - b
     return (_summ), (_razn)
@@ -6,4 +6,4 @@ a=input()
 b=input()
 a=int(a)
 b=int(b)
-print(calculation(a,b))
+print(calculation())
